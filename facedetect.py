61d10c5fcc735cfb5e4c9833b5ff86eeb3f74433
xamfy <spratyaksh22@gmail.com>
2017-09-28 22:39:40 +0530

    Complete the program

---
+++ facedetect.py
@@ -0,0 +1,26 @@
+import cv2
+import sys
+
+imageLocation = sys.argv[1]
+cascaderLocation = "haarcascade_frontalface_default.xml"
+
+faceCascade = cv2.CascadeClassifier(cascaderLocation)
+
+image = cv2.imread(imageLocation)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+faces = faceCascade.detectMultiScale(
+	gray,
+	scaleFactor=1.2,
+	minNeighbors=5,
+	minSize=(30, 30),
+	flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+	)
+
+print("Found {0} faces".format(len(faces)))
+
+for(x,y,w,h) in faces:
+	cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+cv2.imshow("Faces found", image)
+cv2.waitKey(0)
